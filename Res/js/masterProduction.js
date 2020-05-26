4b688f89876d4404a7d4bc550863d481ed60fcad
fachry53 <46189875+fachry53@users.noreply.github.com>
2020-05-26 18:43:51 +0700

    produksi_master_Fix
    Udah bener, tabel bisa nongol

--- Res/js/masterProduction.js
+++ Res/js/masterProduction.js
@@ -1,20 +1,20 @@
 function productsData(){
     $.ajax({
         url: '../../xml/masterDataProduksi.xml',
-        //dataType: 'xml',
+        dataType: 'xml',
 
-        beforeSend: function () {
-            $('#table').html('Loading...');
+        error: function () {
+            $('#table tbody').append('<tr id="status"><td colspan="7" class="text-center">Can not load data</td></tr>'); 
         },
         
         success: function (response) {
-            var producs = $(response).find('dataBarang').children('barang');
+            var products = $(response).find('dataBarang').children('barang');
             
             var i;
 
-            for (i=0; i < producs.length; i++){
+            for (i=0; i < products.length; i++){
                 
-                var data = producs[i];
+                var data = products[i];
                 
                 var kode = $(data).children('kode').text();
                 var nama = $(data).children('nama').text();
@@ -24,56 +24,21 @@ function productsData(){
                 var status = $(data).children('status').text();
                                             
                 $('#table tbody').append('<tr id="status">'+
-                '<td>' + i + '</td>' +
+                '<td>' + (i+1) + '</td>' +
                 '<td>' + kode + '</td>' +
                 '<td>' + nama + '</td>' +
                 '<td>' + kategori + '</td>' +
                 '<td>' + batas + '</td>' +
                 '<td>' + stok + '</td>' +
-                '<td>' + status + '</td>');				
+                '<td>' + status + '</td>'
+                );		
+                
             }
-        },
-
-        error: function () {
-            $('#table tbody').append('<tr id="status"><td colspan="7" class="text-center">Can not load data</td></tr>'); 
         },
        
     })
 };    
 
-function summary(){
-    $.ajax({
-        url: '../../xml/akn.xml',
-        //dataType: 'xml',
-        error: function () {
-          alert('failed to load data...'); 
-        },
-        
-        success: function (response) {
-            var sum = $(response).find('sums');
-                                        
-           
-            var din = $(sum[0]).children('days').text();	
-            var min = $(sum[0]).children('month').text();					
-            var yin = $(sum[0]).children('year').text();					
-            var tin = $(sum[0]).attr('total');
-            
-            $('#dayin').append(din);
-            $('#monthin').html(min);
-            $('#yearin').html(yin);
-            $('#totalin').html(tin);
-
-            var dout = $(sum[1]).children('days').text();	
-            var mout = $(sum[1]).children('month').text();					
-            var yout = $(sum[1]).children('year').text();					
-            
-            $('#dayout').append(dout);
-            $('#monthout').html(mout);
-            $('#yearout').html(yout);
- 
-        },
-    })
-};
 
 
 function time(){
